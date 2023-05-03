c712a5dcbcaae0420dff28589ef15085e9b766c0
Subarna <77785272+Subarna-1@users.noreply.github.com>
2023-05-03 11:09:51 +0530

    first commit

---
+++ Machine_Learning1.py
@@ -0,0 +1,35 @@
+import io
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# Loads the image file into memory
+with io.open('IELTS-template.jpg', 'rb') as image_file:
+    content = image_file.read()
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.annotate_image({
+    'image': image,
+    'features': [
+        {'type': vision.enums.Feature.Type.FACE_DETECTION},
+        {'type': vision.enums.Feature.Type.TEXT_DETECTION},
+        {'type': vision.enums.Feature.Type.OBJECT_LOCALIZATION},
+    ],
+})
+
+# Prints the results
+print('Faces:')
+for face in response.face_annotations:
+    print(' - Joy likelihood: {}'.format(face.joy_likelihood.name))
+
+print('Text:')
+for text in response.text_annotations:
+    print('\n"{}"'.format(text.description))
+
+print('Objects:')
+for obj in response.localized_object_annotations:
+    print('{} (confidence: {})'.format(obj.name, obj.score))
